57462e221ced2e7f6179d2eedb2a1afd9cc851ba
ethanbrews <ethanbrews77@gmail.com>
2020-08-23 19:44:18 +0100

    Now asks before overwriting

--- generate.py
+++ generate.py
@@ -4,23 +4,33 @@ from sys import argv
 import os.path
 
 names = {
-    "BadgeLogo.png": "BadgeLogo.scale-{scale}.png",
-    "SplashScreen.png": "SplashScreen.scale-{scale}.png",
-    "Square44x44Logo.png": "Square44x44Logo.scale-{scale}.png",
-    "Square44x44Logo-badge-unplated.png": "Square44x44Logo.targetsize-{targetsize}_altform-unplated.png",
-    "Square44x44Logo-badge.png": "Square44x44Logo.targetsize-{targetsize}.png",
-    "Square71x71Logo.png": "Square71x71Logo.scale-{scale}.png",
-    "Square150x150Logo.png": "Square150x150Logo.scale-{scale}.png",
-    "Square310x310Logo.png": "Square310x310Logo.scale-{scale}.png",
-    "StoreLogo.png": "StoreLogo.scale-{scale}.png",
-    "Wide310x150Logo.png": "Wide310x150Logo.scale-{scale}.png"
+    "BadgeLogo.png": "BadgeLogo.scale-{scale}.{ext}",
+    "SplashScreen.png": "SplashScreen.scale-{scale}.{ext}",
+    "Square44x44Logo.png": "Square44x44Logo.scale-{scale}.{ext}",
+    "Square44x44Logo-badge-unplated.png": "Square44x44Logo.targetsize-{targetsize}_altform-unplated.{ext}",
+    "Square44x44Logo-badge.png": "Square44x44Logo.targetsize-{targetsize}.{ext}",
+    "Square71x71Logo.png": "Square71x71Logo.scale-{scale}.{ext}",
+    "Square150x150Logo.png": "Square150x150Logo.scale-{scale}.{ext}",
+    "Square310x310Logo.png": "Square310x310Logo.scale-{scale}.{ext}",
+    "StoreLogo.png": "StoreLogo.scale-{scale}.{ext}",
+    "Wide310x150Logo.png": "Wide310x150Logo.scale-{scale}.{ext}"
 }
 
 SCALES = [1, 0.5, 0.375, 0.3125, 0.25]
 SIZES = [256, 48, 32, 24, 16]
 RESAMPLING_METHOD = Image.LANCZOS
 
-def generate_all_scales(input_file: str, output_folder: str, confirm_overwrite: bool = False):
+def ask_bool(q: str, default: bool):
+    while True:
+        i = input(f"{q} ({'Y' if default else 'y'}/{'n' if default else 'N'})")
+        if i.lower() in ["yes", "y", "true"]:
+            return True
+        elif i.lower() in ["no", "n", "false"]:
+            return False
+        elif i == "":
+            return default
+
+def generate_all_scales(input_file: str, output_folder: str, confirm_overwrite: bool = False, ext: str = "jpg"):
     f_name = os.path.basename(input_file)
     template_name = names.get(f_name, None)
     if template_name is None:
@@ -32,16 +42,20 @@ def generate_all_scales(input_file: str, output_folder: str, confirm_overwrite: 
             scale_name = str(int(400*scale))
             im = Image.open(input_file)
             im = im.resize((int(im.size[0]*scale), int(im.size[1]*scale)), RESAMPLING_METHOD)
-            im.save(os.path.join(output_folder, template_name.format(scale=scale_name)))
+            save_path = os.path.join(output_folder, template_name.format(scale=scale_name, ext=ext))
+            if (not confirm_overwrite) or ask_bool(f"Overwrite {os.path.relpath(save_path, os.getcwd())}?", False):
+                im.save(save_path)
     else:
         for size in SIZES:
             im = Image.open(input_file)
             im = im.resize((size, size), RESAMPLING_METHOD)
-            im.save(os.path.join(output_folder, template_name.format(targetsize=size)))
+            save_path = os.path.join(output_folder, template_name.format(targetsize=size, ext=ext))
+            if (not confirm_overwrite) or ask_bool(f"Overwrite {os.path.relpath(save_path, os.getcwd())}?", False):
+                im.save(save_path)
     
-def generate_all(folder: str, output_folder: str, confirm_overwrite: bool = False):
+def generate_all(folder: str, output_folder: str, confirm_overwrite: bool = False, ext: str = "jpg"):
     for file in os.listdir(folder):
-        generate_all_scales(os.path.join(folder, file), output_folder, confirm_overwrite)
+        generate_all_scales(os.path.join(folder, file), output_folder, confirm_overwrite, ext)
     
 def verify_all():
     print("Not implemented!")
@@ -51,26 +65,18 @@ def verify_all():
 def main():
     parser = argparse.ArgumentParser(
         description="Generate all scales of image assets for a UWP application",
-        usage="python generate.py [-h] action [-i inputFolder] [-o outputFolder]",
+        usage="python generate.py [-h] [-i inputFolder] [-o outputFolder] [-e imageFileType] [-f]",
         epilog="Assets must be copied to the Images or Assets folder then added into the project using Visual Studio"
     )
 
     parser.add_argument("-i", "--input", type=str, help="Path to the input folder", default="input")
     parser.add_argument("-o", "--output", type=str, help="Path to the output folder", default="output")
     parser.add_argument("-f", "--force", action="store_true", help="If present, files in the output folder will be overwritten with no confirmation required.")
-    parser.add_argument("action", help="Action to be executed. Options are: 'generate', 'verify'", type=str)
+    parser.add_argument("-e", "--ext", type=str, help="File type extension for the output images (without the '.'). Default is jpg to reduce file size.", default="jpg")
     
     args = parser.parse_args(argv[1:])
     
-    if args.action == "generate":
-        print("Generating assets")
-        generate_all(args.input, args.output, not args.force)
-        print("Action Completed")
-    elif args.action == "verify":
-        print("Verifying assets in output folder are valid for a UWP application")
-        verify_all(args.input)
-    else:
-        print("Invalid action '%s'. Use the --help flag for usage information." % args.action)
+    generate_all(args.input, args.output, not args.force, args.ext)
     
 
 if __name__ == "__main__":
